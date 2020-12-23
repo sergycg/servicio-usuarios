0db210a8bb0a15aedd2a28dd1e935ca882925177
sergycg <sergycg@gmail.com>
2020-12-23 17:05:47 +0100

    first commit

---
+++ src/main/java/com/usuarios/RepositoryConfig.java
@@ -0,0 +1,18 @@
+package com.usuarios;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+import com.usuarios.commons.models.entity.Role;
+import com.usuarios.commons.models.entity.Usuario;
+
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer{
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(Usuario.class, Role.class);
+	}
+}
---
+++ src/main/java/com/usuarios/SpringbootServicioUsuariosApplication.java
@@ -0,0 +1,15 @@
+package com.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@EntityScan({"com.usuarios.commons.models.entity"})
+@SpringBootApplication
+public class SpringbootServicioUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioUsuariosApplication.class, args);
+	}
+
+}
